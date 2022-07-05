830596de9df19cab011a9ad7b250f39bded93f7a
taggertt <taggertt@mail.ru>
2022-07-05 19:08:48 +0500

    add description

--- src/main.java
+++ src/main.java
@@ -1,9 +1,9 @@
 public class main {
     public static void main(String[] args){
-        int account = 100;
-        int currentBonus;
-        int refill = 1100;
-        account = account + refill;
+        int account = 100; // Количество денег на счёт
+        int currentBonus; // Начисляемые бонусы
+        int refill = 1100; // Сумма к пополнению
+        account = account + refill; // Изменения в счёте до начисления бонусов
 
         if (refill>1000) {currentBonus = refill/100; account = account + currentBonus;
         System.out.println("На счёте сейчас - " + account + " рублей. Начислено " + currentBonus + " бонусов.");}
