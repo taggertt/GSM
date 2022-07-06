b1bc3d17c393396db28678b24303812ae68f0ef7
taggertt <taggertt@mail.ru>
2022-07-06 18:34:10 +0500

    rework code

--- src/main.java
+++ src/main.java
@@ -1,13 +1,17 @@
-public class main {
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
+
         int account = 100; // Количество денег на счёт
         int currentBonus; // Начисляемые бонусы
         int refill = 1100; // Сумма к пополнению
         account = account + refill; // Изменения в счёте до начисления бонусов
 
-        if (refill>1000) {currentBonus = refill/100; account = account + currentBonus;
-        System.out.println("На счёте сейчас - " + account + " рублей. Начислено " + currentBonus + " бонусов.");}
-        else {
+        if (refill > 1000) {
+            currentBonus = refill / 100;
+            account = account + currentBonus;
+            System.out.println("На счёте сейчас - " + account + " рублей. Начислено "
+                    + currentBonus + " бонусов.");
+        } else {
             System.out.println("На счёте - " + account + " рублей. Бонусы не начислены.");
         }
     }
